23c8485a0b61d213bf4b8ced2f6aa5b60c65a551
faheem303 <faheemrehmat360@gmail.com>
2025-02-28 18:00:08 +0500

    new sections added

---
+++ src/Home/LiveDemo.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import eranimg from "../assets/EarnOnSec/video.webm";
+
+function LiveDemo() {
+
+  return (
+    <div className="pt-[20px] pb-[50px] ">
+      <div className="max-w-[1200px]  min-h-[454px] px-[46px] border border-[#B0B0B0] rounded-[13px] w-[100%] mx-auto bg-[#fff] p-[30px] space-y-[1rem]">
+        <h3 className="text-center text-[32px] font-[700] leading-[128.889%]">Live Demo</h3>
+        <div className="bg-[#F3F3F3] max-w-[1033px] w-[100%] mx-auto min-h-[615px] rounded-[7.491px] border  border-[#B0B0B0]">
+
+        </div>
+        <div className="flex justify-center">
+        <a
+              href="https://forms.gle/MqeUsLeJPwxt4gfC7"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-[200px]"
+            >
+            <button
+              className="text-white bg-[#E5AE00] px-[12px] hover:text-black hover:bg-transparent text-[14px] font-[800] border border-[#E5AE00] hover:border-[#000] rounded-[8px] max-w-[200px] w-[100%] h-[39px]"
+            >
+              Join Whitelist Now
+            </button>
+            </a>
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default LiveDemo;
