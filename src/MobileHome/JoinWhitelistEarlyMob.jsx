48838b892a7d4ac9925d2f6bb648091c2e8d9529
faheem303 <faheemrehmat360@gmail.com>
2025-03-03 12:45:23 +0500

    mobile fixes

--- src/MobileHome/JoinWhitelistEarlyMob.jsx
+++ src/MobileHome/JoinWhitelistEarlyMob.jsx
@@ -4,7 +4,7 @@ import swpicon from "../assets/wallet/swp.svg";
 
 function JoinWhitelistEarlyMob() {
   return (
-    <div className="py-[50px]" id="join-whitelist">
+    <div className="pb-[24px]" id="join-whitelist">
       <div className="  w-[100%] flex flex-col px-4 gap-6">
         <div className="  flex flex-col justify-center items-center gap-4 py-6    rounded-[12px] border-[#ECECEC] bg-[#F9F9F9]">
           <h2 className="w-[357px] text-[32px] font-[700] text-center tracking-[-0.56px] capitalize leading-normal">
