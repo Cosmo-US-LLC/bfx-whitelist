40ad7a52ba4b949605515a44e01b760243272cc6
faheem303 <faheemrehmat360@gmail.com>
2025-03-07 11:44:58 +0500

    textual and mobile changes

--- src/MobileHome/CreditCardMob.jsx
+++ src/MobileHome/CreditCardMob.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import creditVideo from "../assets/CreditCard/cred.mp4";
+import applepay from "../assets/CreditCard/btnsvg (2).svg";
+import googlelepay from "../assets/CreditCard/btnsvg (1).svg";
 
 function CreditCardMob() {
   const videoRef = useRef(null);
@@ -33,14 +35,14 @@ function CreditCardMob() {
 
   return (
     <div className="pt-[24px] pb-[24px]">
-      <div className="pt-[14px] pb-[31px] px-[15px] space-y-[20px]  w-[90%] mx-auto bg-[#fff]">
+      <div className=" px-[15px] space-y-[20px]  w-[90%] mx-auto bg-[#fff]">
       
-        <div className="w-[100%] p-[40px] border border-[#9D9D9D] min-h-[460px] bg-[#F3F3F3] rounded-[11px] space-y-[20px]">
+        <div className="w-[100%]  min-h-[460px]  space-y-[20px]">
             <h3 className="text-[20.86px] text-center capitalize font-[700] leading-[128%] text-[#181A20] tracking-[-1px]">
             BFX Credit Card
             </h3>
 
-            <div className="w-[100%] items-center flex justify-center rounded-[8px]">
+            <div className="w-[100%] p-[20px] border border-[#9D9D9D] bg-[#F3F3F3] rounded-[11px] items-center flex justify-center rounded-[8px]">
           <video
             width="300"
             height="260"
@@ -55,32 +57,24 @@ function CreditCardMob() {
           </video>
         </div>
 
-            <ul className="list-disc">
-              <li className="text-[15px] border-b ml-3 py-[8px] font-[500] ">Unlimited spending / No limits </li>
-              <li className="text-[15px] border-b ml-3 py-[8px] font-[500] ">Metal or 18 / 24 karat gold plated cards </li>
-              <li className="text-[15px] border-b ml-3 py-[8px] font-[500] ">10,000 USD monthly ATM withdrawal limit </li>
-              <li className="text-[15px] border-b ml-3 py-[8px] font-[500] ">Up to 100,000 USD per single transaction  </li>
-              <li className="text-[15px] border-b ml-3 py-[8px] font-[500] ">Top up with BFX and more than 20+ crypto </li>
-              <li className="text-[15px] border-b ml-3 py-[8px] font-[500] ">BFX and USDT daily rewards available to pay </li>
-              <li className="text-[15px] border-b ml-3 py-[8px] font-[500] ">Accepted at 250 Million Merchant Worldwide </li>
-              <li className="text-[15px] border-b ml-3 py-[8px] font-[500] ">Cards are exclusively available during BFX presale </li>
+            <ul className="list-disc p-[30px] border border-[#9D9D9D] bg-[#F3F3F3] rounded-[11px]">
+              <li className="text-[14px] border-b ml-3 py-[8px] font-[500] ">Unlimited spending / No limits </li>
+              <li className="text-[14px] border-b ml-3 py-[8px] font-[500] ">Metal or 18 / 24 karat gold plated cards </li>
+              <li className="text-[14px] border-b ml-3 py-[8px] font-[500] ">10,000 USD monthly ATM withdrawal limit </li>
+              <li className="text-[14px] border-b ml-3 py-[8px] font-[500] ">Up to 100,000 USD per single transaction  </li>
+              <li className="text-[14px] border-b ml-3 py-[8px] font-[500] ">Top up with BFX and more than 20+ crypto </li>
+              <li className="text-[14px] border-b ml-3 py-[8px] font-[500] ">BFX and USDT daily rewards available to pay </li>
+              <li className="text-[14px] border-b ml-3 py-[8px] font-[500] ">Accepted at 250 Million Merchant Worldwide </li>
+              <li className="text-[14px]  ml-3 pt-[8px] font-[500] ">Cards are exclusively available during BFX presale </li>
             </ul>
           </div>
-       
-        <div className="flex justify-center">
-          <a
-            href="https://forms.gle/MqeUsLeJPwxt4gfC7"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button
-              className="text-white bg-[#E5AE00] px-[12px] hover:text-black hover:bg-transparent text-[14px] font-[800] border border-[#E5AE00] hover:border-[#000] rounded-[8px] max-w-[158px] w-[100%] h-[40px]"
-              onClick={handleScroll}
-            >
-              Join Whitelist Now
-            </button>
-          </a>
-        </div>
+          <div>
+            <p className="text-[14px] font-[400] text-center leading-[150%]">Shipping will begin once the BFX presale ends, as all cards will be sent out simultaneously.  Every BFX Founder’s Club member will receive a secure form after the presale to submit their shipping details. Cards will be delivered worldwide via DHL or FedEx Express. </p>
+          </div>
+          <div className="flex justify-center space-x-3">
+          <button className="w-[70.2px] h-[30px] hover:opacity-[0.7] flex justify-center items-center rounded-[4.8px] border border-[#000]"><img src={applepay} alt="" /></button>
+          <button className="w-[70.2px] h-[30px] hover:opacity-[0.7] flex justify-center items-center rounded-[4.8px] border border-[#000]"><img src={googlelepay} alt="" /></button>
+          </div>
       </div>
     </div>
   );
--- src/MobileHome/MobileHero.jsx
+++ src/MobileHome/MobileHero.jsx
@@ -37,7 +37,7 @@ function MobileHero() {
               The First Crypto <br /> Trading Super App
             </h2>
             <p className="text-[15px] max-w-[865px] w-[100%] mx-auto font-[400] leading-[153.667%] text-center text-[#000] tracking-[-0.32px] ">
-            BlockchainFX is the only crypto native trading platform that offers instant access to the world’s largest financial markets. Users can trade crypto, forex, stocks, and more, all in one place without having to switch brokers. <br /><br /> This ecosystem enables $BFX token holders to earn up to 25,000 USDT daily, receiving rewards every time someone trades on BlockchainFX.
+            BlockchainFX is the only crypto native trading platform that provides instant access to the world’s largest financial markets. Users can trade crypto, forex, stocks, ETFs, futures, options, bonds, indices, CFDs, and more, all in one place. As a community driven ecosystem, BFX token holders earn USDT fees whenever someone trades on the app.
             </p>
           </div>
           <div className="flex justify-center items-center space-x-[17px]">
