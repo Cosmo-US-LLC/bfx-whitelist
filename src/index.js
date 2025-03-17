5ee110a747592b7594d4a71d2e23c8c23c9db984
faheem303 <faheemrehmat360@gmail.com>
2025-03-17 11:09:27 +0500

    translation setup

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <I18nextProvider i18n={i18n}> 
     <App />
+    </I18nextProvider>
   </BrowserRouter>
   </React.StrictMode>
 );
